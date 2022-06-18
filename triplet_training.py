9868528461d2b15b59c4a84b0df6eb70d6ba13be
Harsh Raj <64323122+harshraj172@users.noreply.github.com>
2022-06-18 22:40:44 +0530

    Update triplet_training.py

--- triplet_training.py
+++ triplet_training.py
@@ -205,14 +205,17 @@ if __name__ == "__main__":
 
     parser.add_argument(
         '--train_batch_size',
+        type=int, 
         help='the training batch size, typically, larger is better',
         required=True)
 
     parser.add_argument('--epoch_size',
+                        type=int,
                         help='The number of epoch size',
                         required=True)
 
     parser.add_argument('--samples_per_label',
+                        type=int,
                         help='The number of instances for each concept. ',
                         required=True)
 
