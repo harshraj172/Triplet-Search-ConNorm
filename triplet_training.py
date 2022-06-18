2b9b4edae1866c95cadf11cc4aed5230cf7dc9da
Harsh Raj <64323122+harshraj172@users.noreply.github.com>
2022-06-18 22:37:29 +0530

    Update triplet_training.py

--- triplet_training.py
+++ triplet_training.py
@@ -172,7 +172,6 @@ def model_training(
         train_objectives=[(train_dataloader, train_loss)],
         # evaluator = None,
         evaluator=ir_evaluator_n2c2_dev,
-        output_path_ignore_not_empty=True,
         optimizer_params={
             'lr': 1e-4,
             'eps': 1e-6,
